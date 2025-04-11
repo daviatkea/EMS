915d64bf20761b703605cfd87cc05eb21a0447ae
Dannie Vinther <davi@kea.dk>
2025-04-11 15:07:54 +0200

    Enhance error handling and add server health check endpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Importér routes
 const datesRoutes = require("./routes/dates");
@@ -18,8 +19,20 @@ app.use("/events", eventsRoutes);
 
 // Global fejlhåndtering
 app.use((err, req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, PATCH");
   console.error(err.stack);
   res.status(500).json({ message: "Noget gik galt!" });
+});
+app.get("/", function (req, res) {
+  res.json({
+    isMyServerLive: true,
+    suggestions: "Try visiting /events or /locations",
+  });
 });
 
 app.listen(PORT, () => {
