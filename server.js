6c1a7eaac99190232d49902da4f3976b980a5dfc
Dannie Vinther <davi@kea.dk>
2025-04-11 15:24:34 +0200

    Refactor CORS middleware implementation and update glitch.json include pattern

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+// Global middleware for CORS – sættes tidligt i chain
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,17 +31,6 @@ app.use("/dates", datesRoutes);
 app.use("/locations", locationsRoutes);
 app.use("/events", eventsRoutes);
 
-// Global fejlhåndtering
-app.use((err, req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, PATCH");
-  console.error(err.stack);
-  res.status(500).json({ message: "Noget gik galt!" });
-});
 app.get("/", function (req, res) {
   res.json({
     isMyServerLive: true,
